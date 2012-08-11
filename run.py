4b32a302808bdf89d813392330c460d49ace3326
Cameron Oltmann <cameron.oltmann@gmail.com>
2012-08-10 17:28:55 -0700

    Added episode copy scoring

--- run.py
+++ run.py
@@ -6,6 +6,7 @@ Created on 2012-08-08
 
 from src.settings import Settings
 import os
+import re
 
 def add_episode(shows, ep):
     (series, episode, title, format, path) = ep
@@ -39,6 +40,10 @@ def get_episodes(path, shows = {}):
     return shows
 
 def find_duplicates(shows):
+    '''
+    Takes a shows dictionary as created by get_episodes and returns a sub-dictionary
+        that contains only the episodes with duplicates 
+    '''
     duplicates = {}
     for series in shows:
         for episode in shows[series]:
@@ -48,6 +53,42 @@ def find_duplicates(shows):
                 duplicates[series][episode]=shows[series][episode]
     return duplicates
 
+def score_copies(copies):
+    '''
+    Takes a list of copies for an episode ( [[format, filename], ...] )
+    Appends the score to each episode's list value ( [[format, filename, score], ...] )
+      -  Vertical resolution = starting points
+        - <lines>p = <lines> points
+        - <lins>i = <lines/2> points
+        - hdtv = 320 points
+      - REPACK = +100 points
+      - PROPER = +100 points
+    '''
+    for copy in copies:
+        score = 0
+        tags = copy[0].upper().split()
+        if 'HDTV' in tags:
+            score = 320
+        for tag in tags:
+            if re.match('[0-9]+I', tag):
+                score = int(tag[:-1])/2
+            elif re.match('[0-9]+P', tag):
+                score = int(tag[:-1])
+        if 'REPACK' in tags:
+            score += 100
+        if 'PROPER' in tags:
+            score += 100
+        copy.append(score)
+
+def score_episodes(shows):
+    '''
+    Takes a shows dictionary as created by get_episodes or find_duplicates
+    Adds scores to the individual episode copies via score_duplicates    
+    '''
+    for show in shows:
+        for episode in shows[show]:
+            score_copies(shows[show][episode])
+                
 if __name__ == '__main__':
     config = Settings()
     valid_ext = config.get('Ext').split()
@@ -55,7 +96,10 @@ if __name__ == '__main__':
     config.save()
     shows = get_episodes(config.get('Home'))
     duplicates = find_duplicates(shows)
+    score_episodes(duplicates)
     for show in duplicates:
         print show
         for episode in duplicates[show]:
             print '  ' + episode + ' - ' + str(len(duplicates[show][episode]))
+            for copy in duplicates[show][episode]:
+                print '    ' + str(copy[2]) + '  ' + copy[0] + '  ' + copy[1] 
